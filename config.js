5cfe9e7deb93ab623b682a9107289f455f6966b7
Megaton <90366824+MegatonDev@users.noreply.github.com>
2021-09-09 19:06:39 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 config = {
-    token: "" // Get one here  https://discord.com/developers/applications
+    token: "", // Get one here  https://discord.com/developers/applications
+    prefix: "$" // Your token this is how you will be able to use the commands.
 }
 
 module.exports = config;
