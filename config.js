7b607f9874e2d74a998aa2127ff62ed0621ffd39
Aspect <90366824+AsepctDEVS@users.noreply.github.com>
2021-09-23 01:11:05 -0500

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-config = {
-    token: "", // Get one here  https://discord.com/developers/applications
-    prefix: "$" // Your token this is how you will be able to use the commands.
-}
-
-module.exports = config;
